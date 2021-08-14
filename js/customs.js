82f9a74dc5fece0414484ead8a589b9df1b0d0f1
TsurikViktor <tsurik.viktor.vladimirovich@gmail.com>
2021-08-14 14:29:58 +0300

    commit start

---
+++ js/customs.js
@@ -0,0 +1,9 @@
+var result;
+
+for (var i = 1; i < 11; i++) {
+    for (var j = 1; j < 11; j++) {
+        result = i*j;
+        console.log(`${i} * ${j} = ${result}`);
+    }
+    console.log(`--/--/--/--/--/--`)
+};
